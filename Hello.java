0f91359da05b4d7288c16b255bda0bfd742222a8
nirmal jangid <jangidnirmal15@gmail.com>
2025-02-21 14:34:34 +0530

    Hello

---
+++ Hello.java
@@ -0,0 +1,16 @@
+
+
+public class Hello {
+public static int helloprint(){
+    System.out.println("hello word ");
+    System.out.println("hello word ");
+    System.out.println("hello word ");
+    System.out.println("hello word ");
+    return 0;
+}    
+public static void main(String[] args) {
+  helloprint();
+    
+}
+    
+}
